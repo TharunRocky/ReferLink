b2ca036857f63091fb0a69bd296532ea4d478d35
Tharun <ktharun885@gmail.com>
2025-11-27 13:37:30 +0530

    Added images for SEO

--- app/layout.js
+++ app/layout.js
@@ -23,17 +23,23 @@ export default function RootLayout({ children }) {
       <head>
         <link rel="manifest" href="/manifest.json" />
         <link rel="canonical" href="https://www.referlink.space" />
+        <link rel="icon" href="/icons/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
         <meta name="theme-color" content="#ffffff"/>
         <meta name="mobile-web-app-capable" content="yes"/>
         <meta name="application-name" content="ReferLink"/>
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"/>
         <meta name="theme-color" media="(prefers-color-scheme: light)" content="#ffffff"/>
         <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#1a1a1a"/>
-        <meta property="og.title" content="ReferLink - Private Job Referral Platform" />
+        <meta property="og:title" content="ReferLink - Private Job Referral Platform" />
         <meta property="og:description" content="connect, Share job opportuniites, and request referrals in a trusted communinty."/>
-        <meta property="og.image" content="https://www.referlink.space/icons/logo.png" />
-        <meta property="og.url" content="https://www.referlink.space" />
-        <meta property="og.type" content="website" />
+        <meta property="og:image" content="https://www.referlink.space/icons/logo.png" />
+        <meta property="og:url" content="https://www.referlink.space" />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content="https://www.referlink.space/icons/logo.png" />
         
       </head>
       <body className={inter.className}>
