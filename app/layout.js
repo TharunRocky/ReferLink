8301ce1642eae2bdebac5eeac59c89077c0a25cd
TharunRocky <ktharun885@gmail.com>
2025-12-02 00:51:36 +0530

    Added analytics

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import { Toaster } from 'sonner';
 import { SessionProviderWrapper } from './providers';
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -48,6 +49,7 @@ export default function RootLayout({ children }) {
         <SessionProviderWrapper>
           <ServiceWorkerRegister /> 
           {children}
+          <Analytics />
           <Toaster position="top-right" richColors />
         </SessionProviderWrapper>
       </body>
