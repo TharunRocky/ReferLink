81db1fcb7837534c0e27ccf0597128e8ec442329
TharunRocky <ktharun885@gmail.com>
2025-12-03 00:00:50 +0530

    Updated service workers

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,17 +2,6 @@
 importScripts("https://www.gstatic.com/firebasejs/9.6.10/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/9.6.10/firebase-messaging-compat.js");
 
-firebase.initializeApp({
-  apiKey: "AIzaSyCNIwIQA4IRxyGI-d_93GV6qKR8Bfp-gZ4",
-  authDomain: "jobsearch-b49dd.firebaseapp.com",
-  projectId: "jobsearch-b49dd",
-  messagingSenderId: "1041147721962",
-  appId: "1:1041147721962:web:5ad5113a95eb8ee617bf93"
-});
-
-const messaging = firebase.messaging();
-
-
 self.addEventListener("push", function (event) {
   const payload = event.data?.json() || {};
   const data = payload.data || {};
